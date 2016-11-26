365a4f26570cd1aee4e183849b9f501d9b4178d0
ashley <ashley830204@gmail.com>
2016-11-27 01:04:25 +0800

    modified services/get_search_course.rb

--- services/get_search_course.rb
+++ services/get_search_course.rb
@@ -5,7 +5,8 @@ class GetSearchCourse
   extend Dry::Monads::Either::Mixin
 
   def self.call(params)
-    results = HTTP.get("#{ShareLearningApp.config.ShareLearning_API}/search/"+params[:search_keyword])
+  	keyword = (params[:search_keyword].split(' ')).join("+")
+    results = HTTP.get("#{ShareLearningApp.config.ShareLearning_API}/search/"+keyword)
     Right(results.body)
   rescue
     Left(Error.new('Our servers failed - we are investigating!'))
