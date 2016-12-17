e0eb0e2b071dfda7d4cb563809827869fd052af6
hsing-yu, Chen <andy19933@gmail.com>
2016-12-17 23:00:59 +0800

    Added spec files for course full info page

---
+++ spec/course_full_info_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+require_relative 'spec_helper'
+
+describe 'Course_full_info Page' do
+  before do
+    unless @browser
+      # @headless = Headless.new
+      @browser = Watir::Browser.new
+    end
+  end
+
+  after do
+    @browser.close
+    # @headless.destroy
+  end
+
+  describe 'Page elements' do
+    include PageObject::PageFactory
+
+    it '(HAPPY) should see website features' do
+      # GIVEN: user goes to the course page
+      visit CourseFullInfoPage do |page|
+        # THEN: user should see correct title, header, 'new review' button
+        page.title.must_include 'ShareLearning'
+        page.heading.must_include 'ShareLearning'
+
+        page.new_review?
+      end
+    end
+
+    it '(HAPPY) should see content' do
+      # GIVEN: user goes to the course page
+      visit CourseFullInfoPage do |page|
+        # THEN: user should see the course title, course photo, course
+        # prerequisite (& header), course helpfulness rating (& header),
+        # course difficulty rating (& header), course source (& header),
+        # course information (& header), and course link (& header)
+        page.course_title.length.must_be :>, 0
+        page.course_photo_element.attribute(:src).length.must_be :>, 0
+
+        page.course_prerequisite_header.must_include 'Prerequisite'
+        page.course_helpfulness_header.must_include 'Helpfulness'
+        page.course_difficulty_header.must_include 'Difficulty'
+        page.course_source_header.must_include 'Source'
+        page.course_information_header.must_include 'Information'
+        page.course_link_header.must_include 'Link'
+      end
+    end
+  end
+end
