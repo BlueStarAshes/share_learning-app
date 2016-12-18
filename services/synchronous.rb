274ab267c43baf12d61396d8db23edaa6c86202a
ashley <ashley830204@gmail.com>
2016-12-18 20:04:38 +0800

    add prototype sycnronous service for benchmarking

---
+++ services/synchronous.rb
@@ -0,0 +1,127 @@
+# frozen_string_literal: true
+
+# Gets list of all groups from API
+class Synchronous
+  extend Dry::Monads::Either::Mixin
+  extend Dry::Container::Mixin
+
+  register :validate_course_id, lambda { |params|
+    course_id = params[:course_id]
+
+    if course_id
+      Right(course_id: course_id)
+    else
+      Left(Error.new('Course id cannot be empty'))
+    end
+  }
+
+  register :get_course_basic_info, lambda { |input|
+    begin
+      course_id = input[:course_id]
+      result =
+        HTTP.get(
+          "#{ShareLearningApp.config.SHARE_LEARNING_API}/courses/#{course_id}"
+        )
+
+      Right(input)
+    rescue
+      Left(
+        Error.new(
+          "Unable to retrieve the basic info of Course #{course_id}"
+        )
+      )
+    end
+  }
+
+  register :get_course_prerequisites, lambda { |input|
+    begin
+      course_id = input[:course_id]
+      result =
+        HTTP.get(
+          "#{ShareLearningApp.config.SHARE_LEARNING_API}" \
+          "/course/prerequisite/#{course_id}"
+        )
+
+      Right(input)
+    rescue
+      Left(
+        Error.new(
+          "Unable to retrieve prerequisites of Course #{course_id}"
+        )
+      )
+    end
+  }
+
+  register :get_course_helpfulness_rating, lambda { |input|
+    begin
+      course_id = input[:course_id]
+      result =
+        HTTP.get(
+          "#{ShareLearningApp.config.SHARE_LEARNING_API}" \
+          "/course/helpful/#{course_id}"
+        )
+
+      Right(input)
+    rescue
+      Left(
+        Error.new(
+          "Unable to retrieve helpfulness rating of Course #{course_id}"
+        )
+      )
+    end
+  }
+
+  register :get_course_difficulty_rating, lambda { |input|
+    begin
+      course_id = input[:course_id]
+      result =
+        HTTP.get(
+          "#{ShareLearningApp.config.SHARE_LEARNING_API}" \
+          "/course/difficulty/#{course_id}"
+        )
+
+      Right(input)
+    rescue
+      Left(
+        Error.new(
+          "Unable to retrieve difficulty rating of Course #{course_id}"
+        )
+      )
+    end
+  }
+
+  register :get_course_reviews, lambda { |input|
+    begin
+      course_id = input[:course_id]
+      result =
+        HTTP.get(
+          "#{ShareLearningApp.config.SHARE_LEARNING_API}" \
+          "/course/#{course_id}/reviews"
+        )
+
+      Right(input)
+    rescue
+      Left(
+        Error.new(
+          "Unable to retrieve reviews of Course #{course_id}"
+        )
+      )
+    end
+  }
+
+  register :return_course_full_info, lambda { |input|
+    Right(input[:course_full_info])
+  }
+
+  def self.call(params)
+    Dry.Transaction(container: self) do
+      step :validate_course_id
+      step :get_course_basic_info
+      step :get_course_prerequisites
+      step :get_course_helpfulness_rating
+      step :get_course_difficulty_rating
+      step :get_course_reviews
+      step :return_course_full_info
+    end.call(params)
+  end
+end
