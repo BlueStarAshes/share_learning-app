db931c42babbf2f4e16dcd388d03d26155b1fac4
ashley <ashley830204@gmail.com>
2016-12-18 11:41:12 +0800

    add flash messages as  not found any course

--- controllers/search.rb
+++ controllers/search.rb
@@ -6,12 +6,17 @@ class ShareLearningApp < Sinatra::Base
 
   get "/search" do
     search_keyword = Search.call(params)
-    course_details = GetSearchCourse.call(search_keyword)
-    
+    course_details = GetSearchCourse.call(search_keyword) 
     if course_details.success? 
       results = course_details.value 
-      @data = CourseDetailsView.new(results)        
-      @keyword = search_keyword.output[:search_keyword]
+      @data = CourseDetailsView.new(results)
+      
+      if @data.udacity_count.nonzero? || @data.coursera_count.nonzero? \
+       || @data.youtube_count.nonzero?
+        @keyword = search_keyword.output[:search_keyword]
+      else
+        flash.now[:error] = 'Course not found'
+      end
       slim :search
     else
        flash[:error] = course_details.value.message
